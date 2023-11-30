b114bad22630a5bd061ee3840cd36fed4f851c12
Siddharth Kumar <sidd.1105@gmail.com>
2023-11-30 01:40:51 -0500

    Create Server.tf

---
+++ Server.tf
@@ -0,0 +1,47 @@
+# Terraform Configuration for VPN Server and Windows 10 Instance
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "vpn_server" {
+  ami           = "ami-12345678"
+  instance_type = "t2.micro"
+  subnet_id     = "subnet-12345678"
+  key_name      = "my_keypair"
+  
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "VPN IP: YOUR_VPN_IP_HERE"
+              echo "Username: HOUDINI\\Dcopperfield"
+              echo "Password: tartans@123"
+              # Additional VPN configuration here
+              EOF
+}
+
+resource "aws_instance" "windows_machine" {
+  ami           = "ami-87654321" # Windows 10 AMI ID
+  instance_type = "t2.micro"
+  subnet_id     = "subnet-12345678"
+  key_name      = "my_keypair"
+  
+  provisioner "file" {
+    source      = "scripts/domain_join.ps1"
+    destination = "C:/domain_join.ps1"
+  }
+  
+  connection {
+    type        = "winrm"
+    user        = "Administrator"
+    password    = "AdminPass123!" # Replace with a secure password
+    port        = 5986
+    https       = true
+    timeout     = "10m"
+    insecure    = true
+  }
+  
+  provisioner "remote-exec" {
+    inline = [
+      "powershell.exe -ExecutionPolicy Unrestricted -File C:/domain_join.ps1"
+    ]
+  }
+}
